5e448d66caec195307bd9be6166b6008462c2b0c
jurab <brazdil94@gmail.com>
2025-12-26 12:21:36 +0100

    Add Railway deployment config
    - Add requirements.txt with Django, gunicorn, whitenoise
    - Add Procfile for gunicorn
    - Configure settings for production (env vars for SECRET_KEY, DEBUG, ALLOWED_HOSTS)
    - Add whitenoise for static files serving
    - Add CSRF_TRUSTED_ORIGINS for Railway domains

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn skilltrees.wsgi
--- skilltrees/settings.py
+++ skilltrees/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q@o*4@2ad4(1o5#je#kay2$(d^8=0y!fz7t-vapjf*^xy_8wu6'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-q@o*4@2ad4(1o5#je#kay2$(d^8=0y!fz7t-vapjf*^xy_8wu6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+CSRF_TRUSTED_ORIGINS = [f"https://{host}" for host in ALLOWED_HOSTS if host]
 
 
 # Application definition
@@ -45,6 +47,7 @@ AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
