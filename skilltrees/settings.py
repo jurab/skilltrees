faecde980cd867bef8d04c1732a3f0c447355be3
jurab <brazdil94@gmail.com>
2025-12-26 13:16:59 +0100

    Fix CSRF trusted origins - use separate env var

--- skilltrees/settings.py
+++ skilltrees/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-q@o*4@2ad4(1o5#je#kay
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-CSRF_TRUSTED_ORIGINS = [f"https://{host}" for host in ALLOWED_HOSTS if host]
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'https://localhost').split(',')
 
 
 # Application definition
