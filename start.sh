0d81886ee6a295636942a335d6f7469de84816db
jurab <brazdil94@gmail.com>
2025-12-26 13:07:02 +0100

    Add initial data fixture and load on deploy

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py loaddata skills/fixtures/initial_data.json || true
 gunicorn skilltrees.wsgi --bind 0.0.0.0:${PORT:-8000}
